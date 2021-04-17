166a3e68f5d761926a5dc1da5ae4c132d60c724b
FloTheFlower <florentina.hofbauer@gmail.com>
2021-04-17 09:15:00 -0700

    .

--- start.py
+++ start.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("start.html")
+    items=["soso", "du bist also,..", "ein Mensch?"]
+    out = render_template("start.html", name="ich bin der grosse weisse elefant. denk an mich.", items=items)
+    return out
     
 if __name__ == "__main__":
     app.run(debug=True)
