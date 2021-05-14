763ddc33fc89bcfe05fa0af6f3786d65ab5f2d0a
FloTheFlower <florentina.hofbauer@gmail.com>
2021-05-14 14:04:24 -0700

    .

--- start.py
+++ start.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+# create engine is the connection string. This tells you 
+# pyslite is a pip package that understands how to talk to sqlite. It translates between sql Alchemy and sql lite: so the order is
+# python - sql alchemy - pysqlite - sqllite
+# the engine in this case is the object that knows everything and can talk to the database. It's the main thing. 
+engine = create_engine("postgresql://a@localhost:5432/RainyDay")
+
 @app.route('/')
 def hello_world():
     items=["soso", "du bist also,..", "ein Mensch?"]
@@ -16,3 +23,4 @@ def flo():
     
 if __name__ == "__main__":
     app.run(debug=True)
+
